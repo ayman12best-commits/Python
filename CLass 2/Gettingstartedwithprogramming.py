e8a1a6c6c43594fb879a3d71757f8c1e6b629aa3
ayman12best-commits <ayman12best@gmail.com>
2026-05-17 10:41:17 +0530

    Add files via upload

---
+++ CLass 2/Gettingstartedwithprogramming.py
@@ -0,0 +1,7 @@
+#print statements
+print("THis is the world of python")
+print(12)
+print("2 + 3 = ",2 + 3)
+print("name\tage\tphone number\nFerran\t13\t3817503856\nTommy\t15\t53627439256")
+print("I am learning python",end=" ")
+print("It is very easy to learn")
---
+++ CLass 2/Variables.py
@@ -0,0 +1,8 @@
+name = "Rajesh"
+grade= 7
+print(name)
+print(grade)
+school = "DPS"
+print("school = ",school)
+n = input("enter your name : ")
+print("Welcome",n,"to the world of coding")

6906d04bc3ff23f5ebe576d21866ab7ad4095c87
ayman12best-commits <ayman12best@gmail.com>
2026-05-17 10:49:12 +0530

    Add files via upload

---
+++ Class2homework.py
@@ -0,0 +1,9 @@
+#store birthdays
+
+Farzan = "19/6/2014"
+Ayman = '6/2/2014'
+Thawban = '11/2/2014'
+
+print("Farzan's Birthday",Farzan)
+print("Ayman's Birthday",Ayman)
+print("Thawban's Birthday",Thawban)

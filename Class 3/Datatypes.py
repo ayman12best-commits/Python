8f9a9c7c991c23d7b4b85d14ae2fa4f9f70cc995
ayman12best-commits <ayman12best@gmail.com>
2026-05-23 16:40:12 +0530

    Add files via upload

---
+++ Class 3/Datatypes.py
@@ -0,0 +1,9 @@
+# data types
+a = 34
+print("The type of a is",type(a))
+name = 'codingal'
+print("The type of name is",type(name))
+flag = True
+print("The type of flag is",type(flag))
+height = 67.34
+print("The type of height is",type(height))
---
+++ Class 3/Reverse.py
@@ -0,0 +1,5 @@
+# say the string in reverse direction
+name = input("enter a sentence :")
+print(name)
+print("The reverse is ")
+print(name[::-1])

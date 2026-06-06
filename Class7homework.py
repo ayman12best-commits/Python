31fddccb7460dbc7eed685c66fc6f6ec5b1b70ba
ayman12best-commits <ayman12best@gmail.com>
2026-06-06 11:02:00 +0530

    Add files via upload

---
+++ Class7homework.py
@@ -0,0 +1,10 @@
+# age between 10 -20
+
+age = int(input("enter the age : "))
+if age >= 10:
+    if age <= 20:
+        print("The age is between 10 - 20")
+    else:
+        print("The age is greater than 20")
+else:
+    print("The age is less than 10")
